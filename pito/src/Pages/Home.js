44fbca279b3fb51beceee9856744a9efeb3210fb
Andrew Makris <amakri386@west-mec.org>
2023-01-20 11:35:55 -0700

    push

--- pito/src/Pages/Home.js
+++ pito/src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import image from '../IMG_6552.JPG'
 
 const home = () => {
   return (
     <div className="Home-Container">
-        <p>Professional Youtube/Tiktoker</p>
+        {/* <p>Professional Youtube/Tiktoker</p> */}
+        <img src={image} alt=""/>
         <h1>David Rosen's </h1>
         <div className="Flex-Home">
-            <a href=""><button><h2>Tiktok</h2></button></a>
+            <a href="https://www.tiktok.com/@chosen.rosen?is_from_webapp=1&sender_device=pc"><button><h2>Tiktok</h2></button></a>
             <a href=""><button><h2>Workouts</h2></button></a>
             <a href=""><button><h2>Youtube</h2></button></a>
         </div>
