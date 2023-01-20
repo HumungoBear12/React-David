176ed6e16027c5f00df34a5e55f1e11752f126cc
Andrew Makris <amakri386@west-mec.org>
2023-01-20 11:13:48 -0700

    push

---
+++ pito/src/Pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div className="Home-Container">
+        <p>Professional Youtube/Tiktoker</p>
+        <h1>David Rosen's </h1>
+        <div className="Flex-Home">
+            <a href=""><button><h2>Tiktok</h2></button></a>
+            <a href=""><button><h2>Workouts</h2></button></a>
+            <a href=""><button><h2>Youtube</h2></button></a>
+        </div>
+    </div>
+  )
+}
+
+export default home
