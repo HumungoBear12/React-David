27cd54352582be1157ec30ecb58d18e5837293a9
Andrew Makris <amakri386@west-mec.org>
2023-01-31 10:55:39 -0700

    Updated
    Changed image and the width/height of it
    
    Change location of header
    
    Added youtube hyperlink
    
    Changed color from pink to blue

--- pito/src/Pages/Home.js
+++ pito/src/Pages/Home.js
@@ -5,12 +5,12 @@ const home = () => {
   return (
     <div className="Home-Container">
         {/* <p>Professional Youtube/Tiktoker</p> */}
-        <img src={image} alt=""/>
-        <h1>David Rosen's </h1>
+        <img src="https://wallpaper.dog/large/17051111.jpg" alt=""/>
+        <h1>Chosen Rosen </h1>
         <div className="Flex-Home">
             <a href="https://www.tiktok.com/@chosen.rosen?is_from_webapp=1&sender_device=pc"><button><h2>Tiktok</h2></button></a>
             <a href=""><button><h2>Workouts</h2></button></a>
-            <a href=""><button><h2>Youtube</h2></button></a>
+            <a href="https://www.youtube.com/@Chosen_Rosen"><button><h2>Youtube</h2></button></a>
         </div>
     </div>
   )
